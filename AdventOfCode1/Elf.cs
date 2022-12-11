cb2d81277b7e29eb39784ce7bb367d7879dc21e2
Kieran McGuire <kieran.r.mcguire@gmail.com>
2022-12-10 23:00:00 -0400

    Fix accidentally added, unrequired using declarations.

--- AdventOfCode1/Elf.cs
+++ AdventOfCode1/Elf.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode1
+﻿namespace AdventOfCode1
 {
     /// <summary>
     /// Let's overcomplicate this! Here's a class to represent elves.
